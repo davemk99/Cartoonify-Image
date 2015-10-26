72a99493005d1138a03cad10b453d7807b17d66a
davemk99 <davemk99@users.noreply.github.com>
2015-10-26 23:16:10 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,19 +2,20 @@
 void cartoonifyImage(cv::Mat srcColor, cv::Mat &dst,bool sketch)
 {
 	cv::Mat srcGray;
-	cv::cvtColor(srcColor, srcGray, CV_BGR2GRAY);
-	cv::medianBlur(srcGray, srcGray, 7);
+	cv::cvtColor(srcColor, srcGray, CV_BGR2GRAY);//GrayScale Image
+	cv::medianBlur(srcGray, srcGray, 7);//Removing Noise From Image
 	cv::Size size = srcColor.size();
 	cv::Mat mask = cv::Mat(size, CV_8U);
 	cv::Mat edges = cv::Mat(size, CV_8U);
 	cv::Laplacian(srcGray, edges, CV_8U, 5);
 	cv::threshold(edges, mask, 80, 255, CV_THRESH_BINARY_INV);
 	if(sketch)
-	{
+	{	//this is enough for sketch image
 		cv::cvtColor(mask, dst, CV_GRAY2BGR);
 		return;
 	}
 	else {
+		//Median blur is more fast than bilateral,I would suggest medianBlur for realtiming
 		cv::Mat tmp;
 		cv::medianBlur(srcColor, tmp, 3);
 		cv::medianBlur(tmp, srcColor, 3);
@@ -26,16 +27,16 @@ void cartoonifyImage(cv::Mat srcColor, cv::Mat &dst,bool sketch)
 }
 int main()
 {
-	cv::VideoCapture cap(0);
-	cv::Mat frame;
-	cv::Mat dst;
+	cv::VideoCapture cap(0);//Open VideoCapture Device
+	cv::Mat frame;//frame
+	cv::Mat dst;//Destination Frame
 	
 	for (;;)
 	{
-		cap >> frame;
-		cartoonifyImage(frame, dst,true);
-		cv::imshow("Sketch", dst);
-		std::cout << dst.channels();
-		cv::waitKey(100);
+		cap >> frame;//Read frame from device
+		cartoonifyImage(frame, dst,true);//Cartoonify image using function cartoonifyImage implemented in file
+		cv::imshow("Sketch", dst);//show img
+		
+		cv::waitKey(100);//wait 100ms and then do for loop again
 	}
 }
